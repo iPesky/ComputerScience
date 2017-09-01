a3562a15cce46d901e01659943ad4c00a2c9a606
Thomas Huber <tomsemail2100@gmail.com>
2017-09-01 10:01:29 -0700

    Add files via upload

---
+++ ForPrint.java
@@ -0,0 +1,48 @@
+// Thomas Huber 9/1/17
+
+public class ForPrint {
+    public static void main(String[] args) {
+        for(int line = 1; line < 13; line++) {
+                switch (line) {
+                case 1:  line = 1;
+                        System.out.println("     *     ");
+                        break;
+                case 2:  line = 2;
+                        System.out.println("    ***    ");
+                        break;
+                case 3:  line = 3;
+                        System.out.println("   *****   ");
+                        break;
+                case 4:  line = 4;
+                        System.out.println("  *******  ");
+                        break;
+                case 5:  line = 5;
+                        System.out.println(" ********* ");
+                        break;
+                case 6:  line = 6;
+                        System.out.println("***********");
+                        break;
+                case 7:  line = 7;
+                        System.out.println("***********");
+                        break;
+                case 8:  line = 8;
+                        System.out.println(" ********* ");
+                        break;
+                case 9:  line = 9;
+                        System.out.println("  *******  ");
+                        break;
+                case 10: line = 10;
+                        System.out.println("   *****   ");
+                        break;
+                case 11: line = 11;
+                        System.out.println("    ***    ");
+                        break;
+                case 12: line = 12;
+                        System.out.println("     *     ");
+                        break;
+            }
+        }
+    }
+}
+
+// i mean i used a for loop... 
